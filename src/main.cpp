32a3c7846c48436175e8e7048eacf566f4ec922c
BenMalenfant <benmalenant@gmail.com>
2023-06-01 20:51:42 -0400

    FIR filter tests

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,8 @@
 #define DEBUG 1
 #define DBL_TICK_TRESHOLD 100000 //treshold in microseconds
 
+#define LOWPASS_GAIN 0.007 //LOWER MEANS MORE FILTERING
+
 volatile unsigned long fallingEdgeCount = 0;
 unsigned long startTime = 0;
 unsigned long previousTime = 0;
@@ -54,6 +56,107 @@ void printDebug(unsigned long count, unsigned long elapsedTime, unsigned int cpm
 float calculateUsvPH(unsigned int cpm, float conversionRate);
 void printDebugAverage(void);
 float calculateAverageUntilThresholdExceeded(void);
+double fir_low_pass(double input_sample);
+
+#define N 91   // Number of filter coefficients
+
+// Filter coefficients
+const double h[N] = {
+    0.001867519746044820,
+    0.002275293677911463,
+    0.002692845467883248,
+    0.003119400211540369,
+    0.003554151391578438,
+    0.003996262853518041,
+    0.004444870878876082,
+    0.004899086349965267,
+    0.005357997000181999,
+    0.005820669743355830,
+    0.006286153075466025,
+    0.006753479541784039,
+    0.007221668262275161,
+    0.007689727507889525,
+    0.008156657320192493,
+    0.008621452166627776,
+    0.009083103623574271,
+    0.009540603079249766,
+    0.009992944448431896,
+    0.010439126890909214,
+    0.010878157525543523,
+    0.011309054131818317,
+    0.011730847830767882,
+    0.012142585737226980,
+    0.012543333575412086,
+    0.012932178249941664,
+    0.013308230364524597,
+    0.013670626680692492,
+    0.014018532509122373,
+    0.014351144026291085,
+    0.014667690509420725,
+    0.014967436482914990,
+    0.015249683769748768,
+    0.015513773441556675,
+    0.015759087661469738,
+    0.015985051414072162,
+    0.016191134117190663,
+    0.016376851110586923,
+    0.016541765016997088,
+    0.016685486971350718,
+    0.016807677714403370,
+    0.016908048547430839,
+    0.016986362145057821,
+    0.017042433223728004,
+    0.017076129063764694,
+    0.017087369883420026,
+    0.017076129063764694,
+    0.017042433223728004,
+    0.016986362145057821,
+    0.016908048547430839,
+    0.016807677714403370,
+    0.016685486971350718,
+    0.016541765016997088,
+    0.016376851110586923,
+    0.016191134117190663,
+    0.015985051414072162,
+    0.015759087661469738,
+    0.015513773441556675,
+    0.015249683769748768,
+    0.014967436482914990,
+    0.014667690509420725,
+    0.014351144026291085,
+    0.014018532509122373,
+    0.013670626680692492,
+    0.013308230364524597,
+    0.012932178249941664,
+    0.012543333575412086,
+    0.012142585737226980,
+    0.011730847830767882,
+    0.011309054131818317,
+    0.010878157525543523,
+    0.010439126890909214,
+    0.009992944448431896,
+    0.009540603079249766,
+    0.009083103623574271,
+    0.008621452166627776,
+    0.008156657320192493,
+    0.007689727507889525,
+    0.007221668262275161,
+    0.006753479541784039,
+    0.006286153075466025,
+    0.005820669743355830,
+    0.005357997000181999,
+    0.004899086349965267,
+    0.004444870878876082,
+    0.003996262853518041,
+    0.003554151391578438,
+    0.003119400211540369,
+    0.002692845467883248,
+    0.002275293677911463,
+    0.001867519746044820
+};
+
+// Buffer to store input samples
+double x[N] = {0};
 
 long int last_pulse = 0;
 
@@ -111,7 +214,8 @@ void loop()
   }
 
   float cpm_tmp = MIN_IN_MICRO/(float)pulse_interval;
-  cpm_avg = cpm_avg - 0.007*(cpm_avg - cpm_tmp);
+  //cpm_avg = cpm_avg - LOWPASS_GAIN*(cpm_avg - cpm_tmp);
+  cpm_avg = fir_low_pass(cpm_tmp);
   Serial.println(cpm_avg,10);
   // send the message via ROS Serial
   radiation.data = pulse_interval/1000.0;
@@ -120,6 +224,27 @@ void loop()
 
   while(micros() - timestmp < 100000){
   }
+}
+
+double fir_low_pass(double input_sample)
+{
+    // Shift the input samples in the buffer
+    for (int i = N - 1; i > 0; i--)
+    {
+        x[i] = x[i - 1];
+    }
+
+    // Store the new input sample at the beginning of the buffer
+    x[0] = input_sample;
+
+    // Perform the filtering operation
+    double output_sample = 0;
+    for (int i = 0; i < N; i++)
+    {
+        output_sample += h[i] * x[i];
+    }
+
+    return output_sample;
 }
 
 void printDebug(unsigned long count, unsigned long elapsedTime, unsigned int cpm, float usvPerHour)
